0bb4292dd2d66d9ecbe281b2ba1bcb1ae400ddcf
BennyThenemy <benyyofe@gmail.com>
2024-11-29 14:26:19 +0200

    fixed globalcss and tailwind shit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -43,38 +43,6 @@ const config: Config = {
   			strongText: '#3C3E5F',
   			weakText: '#CCCCCC',
   			mutedText: '#667085',
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
   		},
   		boxShadow: {
   			xxsmall: '0px 1px 2px 0px rgba(0, 0, 0, 0.05)',
@@ -92,11 +60,6 @@ const config: Config = {
   			'xlarge-inner': '0px 24px 48px -12px rgba(0, 0, 0, 0.18) inset',
   			'xxlarge-inner': '0px 32px 64px -12px rgba(0, 0, 0, 0.14) inset'
   		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
   	}
   },
 
