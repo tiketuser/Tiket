17a7e7771201ebf095cbffd292b1ac433feeee3f
Aviv Nir <avivnir18@gmail.com>
2024-12-09 23:46:24 +0200

    fiexd errors for vercel deployment again

--- app/components/CardCarousel/CardCarousel.tsx
+++ app/components/CardCarousel/CardCarousel.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React, { useState } from "react";
 import Card from "../Card/Card";
 import Arrow from "../../../public/images/Home Page/Web/Arrow-1.svg";
 import Image from "next/image";
