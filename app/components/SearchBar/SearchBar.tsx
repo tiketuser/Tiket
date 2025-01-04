2ecc5d804a3e9894afd9c8293a3fa8d68a843639
BennyThenemy <benyyofe@gmail.com>
2025-01-04 22:09:21 +0200

    modifyied search bar

--- app/components/SearchBar/SearchBar.tsx
+++ app/components/SearchBar/SearchBar.tsx
@@ -1,30 +1,26 @@
-import React from 'react'
+import Image from 'next/image';
+import SearchIcon from "../../../public/images/SearchBar/Search Icon.svg";
 
 const SearchBar = () => {
   return (
-    <div className='flex justify-center items-center pt-8 dir="rtl" '>
-        <label className="input input-bordered shadow-lg flex items-center w-full max-w-md">
-            
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="h-4 w-4 opacity-70">
-                <path
-                fillRule="evenodd"
-                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                clipRule="evenodd" />     
-            </svg>
-
-            <input 
-                type="text" 
-                className="grow text-right " 
-                dir="rtl" 
-                placeholder="חפש אירוע" 
-            />
-        </label>
+    <div className="flex justify-center items-center pt-9 sm:mx-0 mx-16">
+      <div className="relative w-full max-w-md">
+        <input
+          type="text"
+          placeholder="חפש אירוע"
+          className="w-full py-3 pl-12 pr-4 rounded-lg border border-gray-300 sm:text-text-medium text-text-small rtl focus:outline-none focus:ring-0 focus:border-gray-300"
+        />
+        <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+          <Image
+            src={SearchIcon}
+            alt="Search Icon"
+            width={24}
+            height={24}
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
