2a6d1f8c5fffe484526c9a9ba58191b9ac3d9479
BennyThenemy <benyyofe@gmail.com>
2025-01-08 23:52:27 +0200

    starting dialogs

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image";
 import React from "react";
 
@@ -5,58 +7,63 @@ import HeartIcon from "../../../public/images/NavBar/Heart.svg";
 import Arrow from "../../../public/images/Home Page/Web/Arrow.svg";
 import ProfileButton from "../../../public/images/Home Page/ProfileButton.svg";
 
+import LoginDialog from "../../components/Dialogs/LoginDialog/LoginDialog"
+
 const NavBar = () => {
+  const [isLoginDialogOpen, setLoginDialogOpen] = React.useState(false);
   return (
-    <div
-      dir="ltr"
-      className="relative navbar bg-white flex justify-between items-center px-4 z-50 h-20"
-    >
-      {/* Left Side */}
-      <div>
-        <h1 className="text-xl font-extrabold">Tiket</h1>
+    <>
+      <div
+        dir="ltr"
+        className="relative navbar bg-white flex justify-between items-center px-4 z-50 h-20"
+      >
+        {/* Left Side */}
+        <h1 className="text-text-large font-bold">Tiket</h1>
+
+        {/* Right Side */}
+        <div className="flex space-x-6">
+          {/* Tickets Button */}
+          <button className="flex items-center space-x-2 rtl:space-x-reverse hover:text-gray-600 focus:outline-none">
+            <Image src={Arrow} alt="Arrow" />
+            <span className="text-text-large font-normal">הכרטיסים שלי</span>
+          </button>
+
+          {/* Like Button */}
+          <button
+            role="btn"
+            className="btn btn-ghost btn-circle avatar hover:bg-red-200"
+          >
+            <Image
+              src={HeartIcon}
+              alt="heart icon"
+              style={{ width: "25px", height: "25px", overflow: "visible" }}
+            />
+          </button>
+
+          {/* Hide These in Mobile */}
+          <button className="hidden sm:flex btn btn-secondary border-primary border-[2px] bg-white w-24 text-primary text-text-large font-normal">
+            הירשם
+          </button>
+          <button className="hidden sm:flex btn btn-primary w-24 text-gray-50 text-text-large font-normal"
+            onClick={() => setLoginDialogOpen(true)}>
+            התחבר
+          </button>
+
+          <button
+            tabIndex={0}
+            role="btn"
+            className="hidden sm:flex btn btn-ghost btn-circle avatar hover:bg-red-100"
+          >
+            <Image
+              src={ProfileButton}
+              alt="Profile"
+              style={{ width: "24px", height: "36px", overflow: "visible" }}
+            />
+          </button>
+        </div>
       </div>
-
-      {/* Right Side */}
-      <div className="flex space-x-6">
-        {/* Tickets Button */}
-        <button className="flex items-center space-x-2 rtl:space-x-reverse hover:text-gray-600 focus:outline-none">
-          <Image src={Arrow} alt="Arrow" />
-          <span className="text-text-large font-normal">הכרטיסים שלי</span>
-        </button>
-
-        {/* Like Button */}
-        <button
-          role="btn"
-          className="btn btn-ghost btn-circle avatar hover:bg-red-200"
-        >
-          <Image
-            src={HeartIcon}
-            alt="heart icon"
-            style={{ width: "25px", height: "25px", overflow: "visible" }}
-          />
-        </button>
-
-        {/* Hide These in Mobile */}
-        <button className="hidden sm:flex btn btn-secondary border-primary border-[2px] bg-white w-24 text-primary text-text-large font-normal">
-          הירשם
-        </button>
-        <button className="hidden sm:flex btn btn-primary w-24 text-gray-50 text-text-large font-normal">
-          התחבר
-        </button>
-
-        <button
-          tabIndex={0}
-          role="btn"
-          className="hidden sm:flex btn btn-ghost btn-circle avatar hover:bg-red-100"
-        >
-          <Image
-            src={ProfileButton}
-            alt="Profile"
-            style={{ width: "24px", height: "36px", overflow: "visible" }}
-          />
-        </button>
-      </div>
-    </div>
+      <LoginDialog isOpen={isLoginDialogOpen} onClose={() => setLoginDialogOpen(false)}/>
+    </>
   );
 };
 
