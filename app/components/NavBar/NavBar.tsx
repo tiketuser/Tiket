482cc4f51634e9ddf9537c10a314777326607693
Aviv Nir <avivnir18@gmail.com>
2025-01-19 09:10:32 +0200

    created some links between pages

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import React from "react";
@@ -7,21 +7,24 @@ import HeartIcon from "../../../public/images/NavBar/Heart.svg";
 import Arrow from "../../../public/images/Home Page/Web/Arrow.svg";
 import ProfileButton from "../../../public/images/Home Page/ProfileButton.svg";
 
-import LoginDialog from "../../components/Dialogs/LoginDialog/LoginDialog"
+import LoginDialog from "../../components/Dialogs/LoginDialog/LoginDialog";
 import SignUpDialog from "../Dialogs/SignUpDialog/SignUpDialog";
+import Link from "next/link";
 
 const NavBar = () => {
   const [isLoginDialogOpen, setLoginDialogOpen] = React.useState(false);
   const [isSignUpDialogOpen, setSignUpDialogOpen] = React.useState(false);
-  
+
   return (
     <>
       <div
         dir="ltr"
         className="relative navbar bg-white flex justify-between items-center px-4 z-50 h-20"
       >
-        {/* Left Side */}
-        <h1 className="text-text-large font-bold">Tiket</h1>
+        {/* Logo */}
+        <Link href="/">
+          <h1 className="text-text-large font-bold">Tiket</h1>
+        </Link>
 
         {/* Right Side */}
         <div className="flex space-x-6">
@@ -32,24 +35,29 @@ const NavBar = () => {
           </button>
 
           {/* Like Button */}
-          <button
-            role="btn"
-            className="btn btn-ghost btn-circle avatar hover:bg-red-200"
-          >
-            <Image
-              src={HeartIcon}
-              alt="heart icon"
-              style={{ width: "25px", height: "25px", overflow: "visible" }}
-            />
-          </button>
-
+          <Link href="/Favorites">
+            <button
+              role="btn"
+              className="btn btn-ghost btn-circle avatar hover:bg-red-200"
+            >
+              <Image
+                src={HeartIcon}
+                alt="heart icon"
+                style={{ width: "25px", height: "25px", overflow: "visible" }}
+              />
+            </button>
+          </Link>
           {/* Hide These in Mobile */}
-          <button className="hidden sm:flex btn btn-secondary border-primary border-[2px] bg-white w-24 text-primary text-text-large font-normal"
-            onClick={() => setSignUpDialogOpen(true)}>
+          <button
+            className="hidden sm:flex btn btn-secondary border-primary border-[2px] bg-white w-24 text-primary text-text-large font-normal"
+            onClick={() => setSignUpDialogOpen(true)}
+          >
             הירשם
           </button>
-          <button className="hidden sm:flex btn btn-primary w-24 text-gray-50 text-text-large font-normal"
-            onClick={() => setLoginDialogOpen(true)}>
+          <button
+            className="hidden sm:flex btn btn-primary w-24 text-gray-50 text-text-large font-normal"
+            onClick={() => setLoginDialogOpen(true)}
+          >
             התחבר
           </button>
 
@@ -66,8 +74,14 @@ const NavBar = () => {
           </button>
         </div>
       </div>
-      <SignUpDialog isOpen={isSignUpDialogOpen} onClose={() => setSignUpDialogOpen(false)} />
-      <LoginDialog isOpen={isLoginDialogOpen} onClose={() => setLoginDialogOpen(false)}/>
+      <SignUpDialog
+        isOpen={isSignUpDialogOpen}
+        onClose={() => setSignUpDialogOpen(false)}
+      />
+      <LoginDialog
+        isOpen={isLoginDialogOpen}
+        onClose={() => setLoginDialogOpen(false)}
+      />
     </>
   );
 };
