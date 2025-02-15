30fe0e8187b38885b207cc3f29681fb904faa3f5
Aviv Nir <avivnir18@gmail.com>
2025-02-15 17:20:10 +0200

    finished adding dropdown to my tickets, still need to add responsiveness

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
 
 import HeartIcon from "../../../public/images/NavBar/Heart.svg";
 import Arrow from "../../../public/images/Home Page/Web/Arrow.svg";
@@ -9,11 +10,37 @@ import ProfileButton from "../../../public/images/Home Page/ProfileButton.svg";
 
 import LoginDialog from "../../components/Dialogs/LoginDialog/LoginDialog";
 import SignUpDialog from "../Dialogs/SignUpDialog/SignUpDialog";
-import Link from "next/link";
 
 const NavBar = () => {
-  const [isLoginDialogOpen, setLoginDialogOpen] = React.useState(false);
-  const [isSignUpDialogOpen, setSignUpDialogOpen] = React.useState(false);
+  const [isLoginDialogOpen, setLoginDialogOpen] = useState(false);
+  const [isSignUpDialogOpen, setSignUpDialogOpen] = useState(false);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [isTouchDevice, setIsTouchDevice] = useState(false);
+  let closeTimeout: NodeJS.Timeout;
+
+  // בדיקה אם זה מסך מגע
+  useEffect(() => {
+    setIsTouchDevice("ontouchstart" in window || navigator.maxTouchPoints > 0);
+  }, []);
+
+  // פתיחה בלחיצה במובייל, וב-hover בדסקטופ
+  const handleDropdownToggle = () => {
+    if (isTouchDevice) {
+      setDropdownOpen((prev) => !prev);
+    }
+  };
+
+  // מניעת flickering על ידי הוספת delay קטן לסגירה
+  const handleMouseLeave = () => {
+    closeTimeout = setTimeout(() => {
+      setDropdownOpen(false);
+    }, 200); // מחכה 200 מילישניות כדי למנוע סגירה מיידית
+  };
+
+  const handleMouseEnter = () => {
+    clearTimeout(closeTimeout);
+    setDropdownOpen(true);
+  };
 
   return (
     <>
@@ -27,14 +54,42 @@ const NavBar = () => {
         </Link>
 
         {/* Right Side */}
-        <div className="flex space-x-6">
-          {/* Tickets Button */}
-          <Link href="/MyTickets">
-            <button className="flex items-center space-x-2 rtl:space-x-reverse hover:text-gray-600 focus:outline-none">
+        <div className="flex space-x-6 relative">
+          {/* Dropdown Parent - מחזיק גם את הכפתור וגם את התפריט */}
+          <div
+            className="relative"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            {/* כפתור ראשי */}
+            <button
+              className="flex items-center space-x-2 rtl:space-x-reverse hover:text-gray-600 focus:outline-none relative"
+              onClick={handleDropdownToggle}
+            >
               <Image src={Arrow} alt="Arrow" />
               <span className="text-text-large font-normal">הכרטיסים שלי</span>
             </button>
-          </Link>
+
+            {/* Dropdown Content */}
+            {isDropdownOpen && (
+              <div
+                className="absolute right-0 w-44 mt-2 bg-white shadow-xxlarge rounded-lg z-50 border border-gray-200"
+                onMouseEnter={handleMouseEnter} // נשאר פתוח כשהעכבר בתוך התפריט
+                onMouseLeave={handleMouseLeave} // מחכה 200ms לפני סגירה
+              >
+                <Link href="/MyTickets">
+                  <div className="px-4 py-2 text-right text-text-medium leading-7 hover:bg-gray-100 cursor-pointer">
+                    אירועים קרובים
+                  </div>
+                </Link>
+                <Link href="/MyListings">
+                  <div className="px-4 py-2 text-right text-text-medium leading-7 hover:bg-gray-100 cursor-pointer">
+                    המודעות שלי
+                  </div>
+                </Link>
+              </div>
+            )}
+          </div>
 
           {/* Like Button */}
           <Link href="/Favorites">
@@ -49,6 +104,7 @@ const NavBar = () => {
               />
             </button>
           </Link>
+
           {/* Hide These in Mobile */}
           <button
             className="hidden sm:flex btn btn-secondary border-primary border-[2px] bg-white w-24 text-primary text-text-large font-normal"
@@ -76,6 +132,7 @@ const NavBar = () => {
           </button>
         </div>
       </div>
+
       <SignUpDialog
         isOpen={isSignUpDialogOpen}
         onClose={() => setSignUpDialogOpen(false)}
