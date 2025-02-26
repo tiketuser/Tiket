5dcccda47eb21d7dbdcd8d1747abe6f6cdfc7125
BennyThenemy <benyyofe@gmail.com>
2025-02-26 18:52:09 +0200

    adding avivs minimalcard to uploud dialogs

--- app/components/Dialogs/UploadTicketDialog/UploadTicketSteps/StepThreeUploadTicket.tsx
+++ app/components/Dialogs/UploadTicketDialog/UploadTicketSteps/StepThreeUploadTicket.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 import { UploadTicketInterface } from "./UploadTicketInterface.types";
 
+import MinimalCard from "@/app/components/MinimalCard/MinimalCard";
 
 const StepThreeUploadTicket: React.FC<UploadTicketInterface> = ({
     nextStep,
@@ -9,7 +10,7 @@ const StepThreeUploadTicket: React.FC<UploadTicketInterface> = ({
 }) => {
     return (
         <div>
-            <div className="w-[518px] mt-12">
+            <div className="w-[668px] mt-12 pr-36">
 
                 {/* Title and Subtitle */}
                 <p className="text-heading-5-desktop font-bold">
@@ -20,20 +21,32 @@ const StepThreeUploadTicket: React.FC<UploadTicketInterface> = ({
                 </p>         
             </div> 
 
-            <p className="text-text-medium font-bold text-strongText">
-                פרט לא תקין? שלח את הכרטיס לבדיקה כדי שנתקן את הפרטים.
-            </p>
+            <div className="mt-8">
+                <MinimalCard 
+                    price={500}
+                    title="עלמה גוב"
+                    date="15 אוק׳"
+                    seatLocation="יציע 4 שורה 24"
+                    width="w-[880px]"
+                />
+            </div>
 
-            <div className="flex flex-col items-center gap-2">
-                <button 
-                    className="btn w-[668px] h-[46px] min-h-0 btn-secondary bg-white text-primary border-primary border-[1px] text-text-large font-normal"
-                >
-                    הוסף כרטיס
-                </button>
+            <div className="w-[600px] mr-36 mt-8">
+                <p className="text-text-medium font-bold text-strongText">
+                    פרט לא תקין? שלח את הכרטיס לבדיקה כדי שנתקן את הפרטים.
+                </p>
 
-                <label className="link link-hover text-primary border-transparent text-text-large font-normal cursor-pointer">
-                    שלח לבדיקה
-                </label>
+                <div className="flex flex-col items-center gap-2 ">
+                    <button 
+                        className="btn w-full h-[46px] min-h-0 btn-secondary bg-white text-primary border-primary border-[1px] text-text-large font-normal"
+                    >
+                        הוסף כרטיס
+                    </button>
+
+                    <label className="link link-hover text-primary border-transparent text-text-large font-normal cursor-pointer">
+                        שלח לבדיקה
+                    </label>
+                </div>
             </div>
 
 
@@ -50,7 +63,7 @@ const StepThreeUploadTicket: React.FC<UploadTicketInterface> = ({
                     className="btn w-[140px] h-[46px] min-h-0 btn-secondary bg-primary text-white text-text-large font-normal disabled:bg-secondary disabled:text-white"
                     onClick={nextStep}
                 >
-                    לשלב הבא   
+                    פרסום כרטיס  
                 </button>
             </div>
         </div>
