a52d385bc148e30bcceaf779319600bdecdc695b
Aviv Nir <avivnir18@gmail.com>
2025-04-12 14:31:08 +0300

    finished responsiveness for my-tickets and my-listings

--- app/MyTickets/page.tsx
+++ app/MyTickets/page.tsx
@@ -57,70 +57,79 @@ const MyTickets = ({ params }: Props) => {
   return (
     <div>
       <NavBar />
-      <TitleSubtitle title="אירועים קרובים" subtitle="אירועים שיתקיימו בקרוב" />
+      <div className="w-full">
+        <TitleSubtitle
+          title="אירועים קרובים"
+          subtitle="אירועים שיתקיימו בקרוב"
+        />
 
-      <div className="pt-14 pr-32 pb-14 pl-32 gap-8 shadow-small-inner">
-        <Image
-          src={ArrowIcon}
-          alt="Arrow icon"
-          onClick={toggleUpcomingCardsVisibility}
-          className={`h-[18px] w-[32px] float-end cursor-pointer transition-transform duration-700 ${
-            showUpcoming ? "rotate-0" : "rotate-180"
-          }`}
-        />
-        <div
-          className={`mt-14 transition-all duration-700 ease-in-out ${
-            showUpcoming ? "opacity-100 h-auto" : "opacity-0 h-0"
-          }`}
-        >
-          {showUpcoming &&
-            cardsData.map((card) => (
-              <div key={card.id} className="flex items-center justify-center">
-                <div className="flex mb-8 w-full justify-center items-center">
-                  <SingleCard
-                    location={card.location}
-                    date={card.date}
-                    title={card.title}
-                    price={card.price}
-                    timeLeft={card.timeLeft}
-                    seatLocation=""
-                    buttonAction="צפייה בכרטיס"
-                  />
+        <div className="pt-5 md:pt-14 px-4 md:px-8 lg:px-32 pb-5 md:pb-14 gap-4 md:gap-8 shadow-small-inner w-full">
+          <Image
+            src={ArrowIcon}
+            alt="Arrow Icon"
+            width={32}
+            height={18}
+            onClick={toggleUpcomingCardsVisibility}
+            className={`w-6 h-7 md:w-8 md:h-5  float-end cursor-pointer transition-transform duration-700 ${
+              showUpcoming ? "rotate-0" : "rotate-180"
+            }`}
+          />
+          <div
+            className={`mt-14 transition-all duration-700 ease-in-out ${
+              showUpcoming ? "opacity-100 h-auto" : "opacity-0 h-0"
+            }`}
+          >
+            {showUpcoming &&
+              cardsData.map((card) => (
+                <div key={card.id} className="flex items-center justify-center">
+                  <div className="flex mb-8 w-full justify-center items-center">
+                    <SingleCard
+                      location={card.location}
+                      date={card.date}
+                      title={card.title}
+                      price={card.price}
+                      timeLeft={card.timeLeft}
+                      buttonAction="צפייה בכרטיס"
+                    />
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+          </div>
         </div>
-      </div>
-      <TitleSubtitle title="היסטוריית רכישות" subtitle="אירועים שהסתיימו" />
-      <div className="pt-14 pr-32 pb-14 pl-32 gap-8 shadow-small-inner">
-        <Image
-          src={ArrowIcon}
-          alt="Arrow icon"
-          onClick={togglePurchasesCardsVisibility}
-          className={`h-[18px] w-[32px] float-end cursor-pointer transition-transform duration-700 ${
-            showPurchases ? "rotate-0" : "rotate-180"
-          }`}
-        />
-        <div
-          className={`transition-all duration-700 ease-in-out mt-14 ${
-            showPurchases ? "opacity-100 h-auto" : "opacity-0 h-0"
-          }`}
-        >
-          {showPurchases &&
-            cardsData.map((card) => (
-              <div key={card.id} className="flex items-center justify-center">
-                <div className="flex mb-10 w-full justify-center items-center">
-                  <SingleCard
-                    location={card.location}
-                    date={card.date}
-                    title={card.title}
-                    price={card.price}
-                    tag="Expired"
-                    buttonAction="צפייה בכרטיס"
-                  />
+
+        <TitleSubtitle title="היסטוריית רכישות" subtitle="אירועים שהסתיימו" />
+        <div className="pt-5 md:pt-14 px-4 md:px-8 lg:px-32 pb-5 md:pb-14 gap-4 md:gap-8 shadow-small-inner">
+          <Image
+            src={ArrowIcon}
+            alt="Arrow Icon"
+            width={32}
+            height={18}
+            onClick={togglePurchasesCardsVisibility}
+            className={`w-6 h-7 md:w-8 md:h-5 float-end cursor-pointer transition-transform duration-700 ${
+              showPurchases ? "rotate-0" : "rotate-180"
+            }`}
+          />
+          <div
+            className={`transition-all duration-700 ease-in-out mt-14 ${
+              showPurchases ? "opacity-100 h-auto" : "opacity-0 h-0"
+            }`}
+          >
+            {showPurchases &&
+              cardsData.map((card) => (
+                <div key={card.id} className="flex items-center justify-center">
+                  <div className="flex mb-10 w-full justify-center items-center">
+                    <SingleCard
+                      location={card.location}
+                      date={card.date}
+                      title={card.title}
+                      price={card.price}
+                      tag="Expired"
+                      buttonAction="צפייה בכרטיס"
+                    />
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+          </div>
         </div>
       </div>
       <Footer />
--- app/components/TitleSubtitle/TitleSubtitle.tsx
+++ app/components/TitleSubtitle/TitleSubtitle.tsx
@@ -6,14 +6,17 @@ interface Props {
 const TitleSubtitle: React.FC<Props> = ({ title, subtitle }) => {
   return (
     <div className="pt-14 pr-6 pb-14 pl-6 shadow-xsmall-inner">
-    <div className="flex justify-center flex-col gap-2 text-center" dir="rtl">
-      <h1 className="text-heading-1-desktop font-bold leading-[68px] text-subtext">
-        {title}
-      </h1>
-      <p className="text-heading-4-desktop font-extrabold leading-[42px] text-strongText">
-        {subtitle}
-      </p>
-    </div>
+      <div
+        className="flex justify-center flex-col sm:gap-2 gap-2 text-center"
+        dir="rtl"
+      >
+        <h1 className="sm:text-heading-1-desktop text-heading-2-mobile font-bold sm:leading-[68px] text-subtext">
+          {title}
+        </h1>
+        <p className="sm:text-heading-4-desktop text-heading-5-mobile  font-extrabold sm:leading-[42px] text-strongText">
+          {subtitle}
+        </p>
+      </div>
     </div>
   );
 };
