bd87fbb13d4d3b1c2117a751163e5236398a5d61
Aviv Nir <avivnir18@gmail.com>
2025-06-27 00:11:04 +0300

    firebase config

---
+++ app/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCGiiy5smPnTFY7RdhsHfe12briESgTr4k",
+  authDomain: "tiket-9268c.firebaseapp.com",
+  projectId: "tiket-9268c",
+  storageBucket: "tiket-9268c.firebasestorage.app",
+  messagingSenderId: "653453593991",
+  appId: "1:653453593991:web:67009ebea86a870f735722"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
